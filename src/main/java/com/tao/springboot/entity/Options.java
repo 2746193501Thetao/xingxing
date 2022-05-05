1dba673c2d09a719f071c3987ad7008ee6eec96e
Thetao <2746193501@qqcom>
2022-05-05 11:18:43 +0800

    第一次提交

--- src/main/java/com/tao/springboot/entity/Options.java
+++ src/main/java/com/tao/springboot/entity/Options.java
@@ -2,4 +2,8 @@ package com.tao.springboot.entity;
 
 public class Options {
     String UserModel;          //用户管理
+
+    public static void main(String[] args) {
+        System.out.println("git1");
+    }
 }
